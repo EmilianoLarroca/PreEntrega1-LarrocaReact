7be181cb27123e71f9153e9a7a41c2f712043204
Emiliano Larroca <emixlarroca05@gmail.com>
2023-09-21 02:12:19 -0300

    rama2

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect , useState} from 'react';
-import ItemDetail from '../ItemDetail/ItemDetail';
+import React, { useEffect , useState} from 'react'
+import ItemDetail from '../ItemDetail/ItemDetail'
 import { useParams } from 'react-router-dom';
 import './itemDetailContainer.css';
-
+import { getItem } from '../../mock/data';
 
  function ItemDetailContainer() {
   
   const [producto, setProducto] = useState({})
   const { id } = useParams()
 
-  // useEffect(()=> {
-  //     getItem(id)
-  //     .then((res)=> setProducto(res))
-  //     .catch((error)=> (error))
-  // },[])
+  useEffect(()=> {
+      getItem(id)
+      .then((res)=> setProducto(res))
+      .catch((error)=> (error))
+  },[])
 
   return (
     <div className='itemListContainerCss'>
